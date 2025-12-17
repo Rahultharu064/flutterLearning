b37fd3f328f086c63d3d903e903cf5b82b388f10
Rahultharu064 <rahultharu46882@icloud.com>
2025-12-17 22:35:07 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/example/learningfultter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.learningfultter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ test/widget_test.dart
@@ -0,0 +1,32 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:learningfultter/main.dart';
+
+void main() {
+  testWidgets('Bit widget smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const Bit());
+
+    // Verify that the app bar title is displayed
+    expect(find.text('Discover'), findsOneWidget);
+    
+    // Verify that the text content is displayed
+    expect(find.text('fsdf'), findsOneWidget);
+    
+    // Verify that buttons are displayed
+    expect(find.text('login'), findsOneWidget);
+    expect(find.text('Outline'), findsOneWidget);
+    expect(find.text('Text button'), findsOneWidget);
+    
+    // Verify that the add icon is present
+    expect(find.byIcon(Icons.add), findsOneWidget);
+  });
+}
